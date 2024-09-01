046ccecdbfa5e3e533434b65135bf7b38b0088c9
1337fury <elouardyabdelhalim@gmail.com>
2024-09-01 19:40:19 +0100

    Add fronted setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { Route, Routes } from "react-router-dom"
+import HomePage from "./pages/HomePage"
+import LoginPage from "./pages/LoginPage"
+import SignupPage from "./pages/SignupPage"
+
+function App() {
+  return (
+	<Routes>
+		<Route path="/" element={<HomePage />} />
+		<Route path="/login" element={<LoginPage />} />
+		<Route path="/signup" element={<SignupPage />} />
+	</Routes>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function HomePage() {
+  return (
+	<div className="hero-bg h-screen">
+		<h1>Home Page</h1>
+	</div>
+  )
+}
