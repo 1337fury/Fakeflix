b54c31f8e712958dc8b223e6a6438439388c9e70
1337fury <elouardyabdelhalim@gmail.com>
2024-09-02 13:52:52 +0100

    Add footer component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,18 @@ import { Route, Routes } from "react-router-dom"
 import HomePage from "./pages/home/HomePage"
 import LoginPage from "./pages/LoginPage"
 import SignupPage from "./pages/SignupPage"
+import Footer from "./components/Footer"
 
 function App() {
   return (
-	<Routes>
-		<Route path="/" element={<HomePage />} />
-		<Route path="/login" element={<LoginPage />} />
-		<Route path="/signup" element={<SignupPage />} />
-	</Routes>
+	<>
+		<Routes>
+			<Route path="/" element={<HomePage />} />
+			<Route path="/login" element={<LoginPage />} />
+			<Route path="/signup" element={<SignupPage />} />
+		</Routes>
+		<Footer />
+	</>
   )
 }
 
