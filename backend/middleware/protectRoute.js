b4d909ec8adc4a9dd04e832f7a410db569305174
1337fury <elouardyabdelhalim@gmail.com>
2024-09-03 12:05:56 +0100

    Hide password from user object

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -16,7 +16,7 @@ export const protectRoute = async (req, res, next) => {
 			return res.status(401).json({ success: false, message: "Unauthorized" });
 		}
 
-		const user = await User.findById(decoded.userId);
+		const user = await User.findById(decoded.userId).select("-password");
 
 		if (!user) {
 			return res.status(401).json({ success: false, message: "Unauthorized" });
