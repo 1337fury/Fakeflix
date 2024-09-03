5768a99158316fbb956dffd2192a9830abc16d8e
1337fury <elouardyabdelhalim@gmail.com>
2024-09-03 12:05:01 +0100

    Add authCheck controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -109,3 +109,12 @@ export async function signout(req, res) {
 		res.status(500).json({ success: false, message: "Internal server error" });
 	}
 }
+
+export async function authCheck(req, res) {
+	try {
+		res.status(200).json({ success: true, user: req.user });
+	} catch (error) {
+		console.error("Error in authCheck controller:", error.message);
+		res.status(500).json({ success: false, message: "Internal server error" });
+	}
+}
