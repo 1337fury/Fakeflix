bec3478754dee5951fe119600435bf6a168a7313
1337fury <elouardyabdelhalim@gmail.com>
2024-09-03 19:16:13 +0100

    Add the auth check functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,20 +4,38 @@ import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
 import Footer from "./components/Footer";
 import { Toaster } from "react-hot-toast";
+import { useAuthStore } from "./store/authUser";
+import { useEffect } from "react";
+import { Navigate } from "react-router";
+import { Loader } from "lucide-react";
 
 function App() {
-  return (
-	<>
-		<Routes>
-			<Route path="/" element={<HomePage />} />
-			<Route path="/login" element={<LoginPage />} />
-			<Route path="/signup" element={<SignupPage />} />
-		</Routes>
-		<Footer />
+	const { user, isCheckingAuth, authCheck } = useAuthStore();
+	console.log(user);
+	useEffect(() => {
+		authCheck();
+	}, [authCheck]);
 
-		<Toaster />
-	</>
-  )
+	if (isCheckingAuth) {
+		return (
+			<div className="h-screen flex justify-center items-center bg-black">
+				<Loader className="size-10 text-red-500 animate-spin" />
+			</div>
+		);
+	}
+
+	return (
+		<>
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+				<Route path="/login" element={!user ? <LoginPage /> : <Navigate to={"/"} />} />
+				<Route path="/signup" element={!user ? <SignupPage /> : <Navigate to={"/"} />} />
+			</Routes>
+			<Footer />
+
+			<Toaster />
+		</>
+	)
 }
 
 export default App
