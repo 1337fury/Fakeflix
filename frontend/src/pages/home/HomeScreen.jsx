c2e832cee40abbbcdeb804cb402b42a55bab5d01
1337fury <elouardyabdelhalim@gmail.com>
2024-09-04 16:37:48 +0100

    Add a loading shimmer effect to the home screen and make Links dynamic

--- frontend/src/pages/home/HomeScreen.jsx
+++ frontend/src/pages/home/HomeScreen.jsx
@@ -3,16 +3,29 @@ import { Link } from 'react-router-dom';
 import { useAuthStore } from '../../store/authUser';
 import Navbar  from '../../components/Navbar';
 import { Play, Info } from 'lucide-react';
+import useTrContent from '../../hooks/useTrContent';
+import { ORIGINAL_IMG_BASE_URL } from '../../utils/constants';
 
 export default function HomeScreen() {
 	const { Logout } = useAuthStore();
+	const { trendingContent } = useTrContent();
+
+	if (!trendingContent)
+		return (
+			<div className='h-screen text-white relative'>
+				<Navbar />
+				<div className='absolute top-0 left-0 w-full h-full bg-black/70 flex items-center justify-center -z-10 shimmer' />
+			</div>
+		);
+
 	return (
 		<>
 			<div className='relative h-screen w-full text-white'>
 				<Navbar />
 
-				<img src='/3wiwel.webp' alt='Hero Background' 
-				     className='absolute top-0 left-0 w-full h-full object-cover -z-50 ' />
+				<img src={ORIGINAL_IMG_BASE_URL + trendingContent?.backdrop_path} 
+					alt='Hero Background' 
+				    className='absolute top-0 left-0 w-full h-full object-cover -z-50 ' />
 
 				<div className='absolute top-0 left-0 w-full h-full bg-black/50 -z-50' aria-hidden="true" />
 
@@ -21,19 +34,21 @@ export default function HomeScreen() {
 
 					<div className='max-w-2xl'>
 						<h1 className='mt-4 text-6xl font-extrabold text-balance'>
-							1337 MED
+							{trendingContent?.title || trendingContent?.name || trendingContent?.original_name}
 						</h1>
 						<p className='mt-4 text-lg'>
-							2022 | 18+
+							{trendingContent?.release_date?.split("-")[0] || trendingContent?.first_air_date?.split("-")[0]} | {trendingContent?.adult ? "18+" : "13+"}
 						</p>
 						<p className='mt-4 text-lg'>
-						1337 is the first to provide IT training in Morocco, completely free of charge, open and accessible to anyone between the ages of 18 and 30. No need for an IT degree, or of having undergone extensive IT training. The only criteria for admission in Treize, Trente-Sept is CREATIVITY.
+							{trendingContent?.overview.length > 200
+								? trendingContent?.overview.slice(0, 250) + "..."
+								: trendingContent?.overview}
 						</p>
 					</div>
 
 					<div className='mt-8 flex'>
 						<Link
-								to={`/watch/100`}
+								to={`/watch/${trendingContent?.id}`} 
 								className='bg-white hover:bg-white/80 text-black font-bold py-2 px-4 rounded mr-4 flex
 								items-center'
 							>
@@ -42,7 +57,7 @@ export default function HomeScreen() {
 							</Link>
 
 							<Link
-								to={`/watch/100`}
+								to={`/watch/${trendingContent?.id}`}
 								className='bg-gray-500/70 hover:bg-gray-500 text-white py-2 px-4 rounded flex items-center'
 							>
 								<Info className='size-6 mr-2' />
