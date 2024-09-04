e4cf7e50bb2433039c87842440fb08a6e33a4c8d
1337fury <elouardyabdelhalim@gmail.com>
2024-09-04 16:40:19 +0100

    Update proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,9 @@ export default defineConfig({
   plugins: [react()],
   server: {
 	proxy: {
-	  '/api': 'http://localhost:5000',
+		"/api": {
+			target: "http://localhost:5000",
+		},
 	},
   },
 })
