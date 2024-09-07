bcaf01afcbfa4804c0ebc4a00a328e69a38af0f9
1337fury <elouardyabdelhalim@gmail.com>
2024-09-07 18:41:08 +0100

    Refactor Movie controllers to improve filtering logic and error handling

--- backend/controllers/movie.controller.js
+++ backend/controllers/movie.controller.js
@@ -1,18 +1,22 @@
 import { getFromIMDB } from "../services/tmdb.service.js";
-import { IDS_TO_EXCULDE } from "../../frontend/src/utils/constants.js";
+import { IDS_TO_EXCULDE } from "../utils/constants.js";
+import { filterContent } from "../utils/helpers.js";
 
 export const getTrendingMovie = async (req, res) => {
 	try {
 		const data = await getFromIMDB("https://api.themoviedb.org/3/trending/movie/day?language=en-US");
 
-		data.results = data.results.filter((movie) => 
-			!movie?.adult && 
-			!IDS_TO_EXCULDE.includes(movie?.id)
-		);
+		if (data.results.length > 0) {
+			const filteredResults = filterContent(data.results);
+	  
+			if (filteredResults.length === 0)
+				return res.status(404).json({ success: false, message: "No appropriate movies found" });
 
-		const randomMovie = data.results[Math.floor(Math.random() * data.results.length)];
+			const randomMovie = filteredResults[Math.floor(Math.random() * filteredResults.length)];
 
-		res.status(200).json({ success: true, content: randomMovie });
+			res.status(200).json({ success: true, content: randomMovie });
+		} else
+			res.status(404).json({ success: false, message: "Movies not found" });
 	} catch (error) {
 		console.error("Error in getTrendingMovie controller:", error.message);
 		res.status(500).json({ success: false, message: "Internal server error" });
@@ -62,12 +66,15 @@ export const getSimilarMovies = async (req, res) => {
 		const { id } = req.params;
 		const data = await getFromIMDB(`https://api.themoviedb.org/3/movie/${id}/similar?language=en-US`);
 
-		data.results = data.results.filter((movie) => 
-			!movie?.adult && 
-			!IDS_TO_EXCULDE.includes(movie?.id)
-		);
+		if (data.results.length > 0) {
+			const filteredResults = filterContent(data.results);
+	  
+			if (filteredResults.length === 0)
+				return res.status(404).json({ success: false, message: "No appropriate movies found" });
 
-		res.status(200).json({ success: true, content: data.results });
+			return res.status(200).json({ success: true, content: filteredResults });
+		} else
+			return res.status(404).json({ success: false, message: "Movies not found" });
 	} catch (error) {
 		if (error.response.status === 404) {
 			return res.status(404).json({ success: false, message: "Movies not found" });
@@ -82,12 +89,15 @@ export const getMoviesByCategory = async (req, res) => {
 		const { category } = req.params;
 		const data = await getFromIMDB(`https://api.themoviedb.org/3/movie/${category}?language=en-US&page=1`);
 
-		data.results = data.results.filter((movie) => 
-			!movie?.adult && 
-			!IDS_TO_EXCULDE.includes(movie?.id)
-		);
+		if (data.results.length > 0) {
+			const filteredResults = filterContent(data.results);
+	  
+			if (filteredResults.length === 0)
+				return res.status(404).json({ success: false, message: "No appropriate movies found" });
 
-		res.status(200).json({ success: true, content: data.results });
+			return res.status(200).json({ success: true, content: filteredResults });
+		} else
+			return res.status(404).json({ success: false, message: "Movies not found" });
 	} catch (error) {
 		if (error.response.status === 404) {
 			return res.status(404).json({ success: false, message: "Movies not found" });
